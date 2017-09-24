79e6a810a5202c6884e8d3ebb35659d2a2c1452a
Kevin Faust <ikevinfaust@gmail.com>
2017-09-23 21:20:05 -0400

    flask stuff

---
+++ static/js/app.js
@@ -0,0 +1,57 @@
+$('select').material_select();
+
+$('#question1nextbtn').on('click', function () {
+    $('#question1').css('display', 'none');
+    $('#question1nextbtn').css('display', 'none');
+    $('#question2').css('display', 'inline');
+    $('#question2nextbtn').css('display', 'inline-block');
+});
+
+$('#question2nextbtn').on('click', function () {
+    $('#question2').css('display', 'none');
+    $('#question2nextbtn').css('display', 'none');
+    $('#question3').css('display', 'inline');
+    $('#question3nextbtn').css('display', 'inline-block');
+});
+
+$('#question3nextbtn').on('click', function () {
+    $('#question3').css('display', 'none');
+    $('#question3nextbtn').css('display', 'none');
+    $('#question4').css('display', 'inline');
+    $('#question4nextbtn').css('display', 'inline-block');
+});
+
+$('#question4nextbtn').on('click', function () {
+    $('#question4').css('display', 'none');
+    $('#question4nextbtn').css('display', 'none');
+    $('#question5').css('display', 'inline');
+    $('#question5nextbtn').css('display', 'inline-block');
+});
+
+$('#question5nextbtn').on('click', function () {
+    $('#question5').css('display', 'none');
+    $('#question5nextbtn').css('display', 'none');
+    $('#question6').css('display', 'inline');
+    $('#question6nextbtn').css('display', 'inline-block');
+});
+
+$('#question6nextbtn').on('click', function () {
+    $('#question6').css('display', 'none');
+    $('#question6nextbtn').css('display', 'none');
+    $('#question7').css('display', 'inline');
+    $('#question7nextbtn').css('display', 'inline-block');
+});
+
+$('#question7nextbtn').on('click', function () {
+    $('#question7').css('display', 'none');
+    $('#question7nextbtn').css('display', 'none');
+    $('#question8').css('display', 'inline');
+    $('#question8nextbtn').css('display', 'inline-block');
+});
+
+$('#question8nextbtn').on('click', function () {
+    $('#question8').css('display', 'none');
+    $('#question8nextbtn').css('display', 'none');
+    $('#question9').css('display', 'inline');
+    $('#question9nextbtn').css('display', 'inline-block');
+});
---
+++ temporary.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request
+import sys, os
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        general_skills_options = []
+        technical_skills_options = []
+        tools_options = []
+        years_of_experience_options = []
+        category_options = []
+        work_env_options = []
+        edu_options = []
+        location_options = []
+        job_type_options = []
+
+        options = [general_skills_options, technical_skills_options, tools_options,
+                   years_of_experience_options, category_options, work_env_options,
+                   edu_options, location_options, job_type_options]
+
+        files = ['General skills.txt', 'Technical skills.txt', 'Languages.txt',
+                 'yearsOf Experience.txt', 'Category.txt', 'Environment.txt',
+                 'Education.txt', 'Location.txt', 'JobType.txt']
+
+        for i in range(len(files)):
+
+            with open(os.path.join(app.root_path, files[i]), 'r') as f:
+                lines = f.readlines()
+            for line in lines:
+                line = line.strip('\n')
+                if len(line) == 0:
+                    continue
+                options[i].append(line)
+
+        return render_template('index.html', options=options)
+
+    elif request.method == 'POST':
+
+        # one long list containing all the column values
+        files = ['General skills.txt', 'Technical skills.txt', 'Languages.txt',
+                 'yearsOf Experience.txt', 'Category.txt', 'Environment.txt',
+                 'Education.txt', 'Location.txt', 'JobType.txt']
+
+        columns = []
+        for i in range(len(files)):
+            with open(os.path.join(app.root_path, files[i]), 'r') as f:
+                lines = f.readlines()
+            for line in lines:
+                line = line.strip('\n')
+                if len(line) == 0:
+                    continue
+                columns.append(line)
+
+        user_features = pd.DataFrame(0, index=np.zeros(1), columns=columns)
+        keywords=[]
+
+        for i in range(1,10):
+            selections=request.form.getlist('question' + str(i))
+            for selection in selections:
+                # assign 1 in pandas dataframe
+                user_features[selection] = 1
+                # add ONLY TOOLS keywords
+                if i == 3:
+                    keywords.append(selection)
+
+        # feed user_features into machine learning model
+        ml_data = user_features.as_matrix()
+
+        # keywords the user chose
+        return str(keywords)
+
+
+if __name__ == '__main__':
+    app.run()
